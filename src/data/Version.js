eab79791407f196b6195aedaf8f3b09cce1c484e
wwwouaiebe <github.christian@guyette.be>
2022-06-04 17:11:33 +0200

    Startin v3.7.0-dev

--- src/data/Version.js
+++ src/data/Version.js
@@ -70,7 +70,7 @@ The current version of TravelNotes
 */
 /* ------------------------------------------------------------------------------------------------------------------------- */
 
-const theAppVersion = 'v3.6.0';
+const theAppVersion = 'v3.7.0-dev';
 
 export { theDataVersion, theAppVersion };
 
