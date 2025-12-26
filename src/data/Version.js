ada9e85994455798f8f941ff096c7dbc5a11f2b5
Christian <wwwouaiebe@users.noreply.github.com>
2025-12-26 17:39:28 +0100

    Fixes on v4.3.9

--- src/data/Version.js
+++ src/data/Version.js
@@ -64,7 +64,7 @@ The current version of TravelNotes
 */
 /* ------------------------------------------------------------------------------------------------------------------------- */
 
-const theAppVersion = 'v4.3.9-dev';
+const theAppVersion = 'v4.3.9';
 
 export { theDataVersion, theAppVersion };
 
